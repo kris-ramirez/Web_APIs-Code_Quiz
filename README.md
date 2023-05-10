8010067ba24fb64e7f1690b2b7cffabb1dfe47af
Kristian Ramirez <Kristian@sids-MacBook-Pro-2.local>
2023-05-10 17:57:18 -0500

    Wednesday changes. Quiz is functional but does not give high scores.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Bootcamp Challenge 3: Web APIs Challenge: Code Quiz
 
 
-
+I chose to build another html rather than use the hide feature because I felt it was easier to style my quiz 
 
 https://www.microfocus.com/documentation/silk-test/200/en/silktestworkbench-help-en/SILKTEST-21EEFF3F-DIFFERENCEBETWEENTEXTCONTENTSINNERTEXTINNERHTML-REF.html
 
--- assets/js/quiz.js
+++ assets/js/quiz.js
@@ -1,88 +1,145 @@
-var startButton = document.querySelector(".start-button");
-var question = Array.from(document.querySelector('.question'));
-var choices = document.querySelectorAll('.answers div');
-var score = document.querySelector('#score');
+var question = document.querySelector('#question');
+var choices = document.querySelectorAll('.answertext');
+var scoreText = document.querySelector('#score');
 var timer1 = document.querySelector('#timer');
 
 var currentQuestion = {};
-var score;
+var score = 0;
 var questioncounter = 0;
 var correctanswers = true;
 var allquestions = [];
-var timeLeft = 100;
+var timeLeft = 1000;
 var maxquestions = 5;
+var timerInterval;
 
 var questions = [
     {
         question: 'Commonly used data types DO Not include:',
-        choices: ['A. strings', 'B. booleans', 'C. alerts', 'D. numbers'],
-        answer: 'alerts'
+        choiceA: 'A. strings',
+        choiceB: 'B. booleans',
+        choiceC: 'C. alerts',
+        choiceD: 'D. numbers',
+        answer: 'C',
     },
     {
         question: 'The condition in an if/else statment is enclosed with _________.',
-        choices: ['quotes', 'curly brackets', 'parenthesis', 'square brackets'],
-        answer: 'curly brackets'
+        choiceA: 'quotes',
+        choiceB: 'curly brackets',
+        choiceC: 'parenthesis',
+        choiceD: 'square brackets',
+        answer: 'B',
     },
     {
         question: 'Arrays in JavaScript can be used to store ___________.',
-        choices: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
-        answer: 'all of the above'
+        choiceA: 'numbers and strings',
+        choiceB: 'other arrays',
+        choiceC: 'booleans',
+        choiceD: 'all of the above',
+        answer: 'D',
     },
     {
         question: 'String values must be enclosed within __________ when being assigned to variables.',
-        choices: ['commas', 'curly brackets', 'quotes', 'paranthesis'],
-        answer: 'quotes'
+        choiceA: 'commas',
+        choiceB: 'curly brackets',
+        choiceC: 'quotes',
+        choiceD: 'paranthesis',
+        answer: 'C',
     },
     {
         question: 'A very useful tool used during development and debugging for printing content to the debugger is: ',
-        choices: ['JavaScript', 'terminal/bash', 'for loops', 'console.log'],
-        answer: 'console.log'
-    },
-]
+        choiceA: 'JavaScript',
+        choiceB: 'terminal/bash',
+        choiceC: 'console.log',
+        choiceD: 'for loops',
+        answer: 'C',
+    }, ]
 
+var startPoints = 100; 
 //when 'start' button clicked run this function
-function startQuiz() {
-    score = 0;
-    allquestions = [...questions]
+startQuiz = () => {
+    questioncounter = 0;
+    points = 0;
+    allquestions = [...questions];
     getNextQuestion();
 }
 
-function getNextQuestion() {
-    if(questioncounter >= maxquestions) {
-        endQuiz(); 
+function getNextQuestion () {
+    if (allquestions.length === 0 || questioncounter > maxquestions) {
+        localStorage.setItem('mostrecentscore', points)
+        document.getElementById('flood').style.display="none";
+        document.getElementById('dry').style.display='block';
+        clearInterval(timerInterval);
+        //return window.location.assign("end.html")
     }
+    questioncounter++;
     //grabs a random question from array
     var qindex = Math.floor(Math.random() * allquestions.length);
-    var currentQuestion = allquestions[qindex];
-    //shows question in html inner is better than textContent b/c 
+    currentQuestion = allquestions[qindex];
+    //shows question in html inner is better than textContent 
     question.innerText = currentQuestion.question;
+
     //shows answer choices
-    choices.forEach((choice, qindex) => {
-    choice.innerText = currentQuestion.choices[qindex];
+    choices.forEach(choice => {
+        var letter = choice.dataset['letter']
+        choice.innerText = currentQuestion['choice' + letter];
+        choice.addEventListener('click', answerSelected);
+        choice.classList.remove('incorrect', 'correct');
     });
-    questioncounter++;
+
+    allquestions.splice(qindex, 1);
     correctanswers = true;
-    
+
 }
+
 //what happens when answers are clicked on
-choices.forEach(choice => {
-    choice.addEventListener('click', e => {
-        correctanswers = false;
-        var selected = e.target;
-        var clickedAnswer = selected.textContent;
-        
-        var classToApply = clickedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
-        selected.classList.add(classToApply);
-    })
+function answerSelected (e) {
+    if (!correctanswers) {
+        return;
+    }
 
-})
-//Take user to high scores page
-function endQuiz () {
+    correctanswers = false;
+    var selected = e.target;
+    var clickedAnswer = selected.dataset['letter'];
+
+    var classToApply = clickedAnswer == currentQuestion.answer ? 'correct' : 'incorrect';
+    if (classToApply === 'correct') {
+        incrementPoints(startPoints);
+    } else {
+        timeLeft -= 10;
+    }
+
+    selected.classList.add(classToApply);
+
+    choices.forEach(choice => {
+        if (choice.dataset['letter'] == currentQuestion.answer) {
+            choice.classList.add('correct');
+        setTimeout(() => {
+        },3000)
+        }
+
+    });
+
+    setTimeout(getNextQuestion, 1000);
 
 }
 
 
+function incrementPoints (num) {
+    points += num;
+    scoreText.innerText = points;
+}
 
+function setTime() {
+    timerInterval = setInterval(function() {
+        timeLeft--;
+        timer1.textContent = timeLeft + ' seconds left';
 
-// Attach event listener to start button to call startQuiz function on click
-startButton.addEventListener("click", startQuiz);
+        if(timeLeft === 0) {
+            clearInterval(timerInterval);
+        }
+    }, 1000);
+}
+setTime();
+startQuiz();
+
+//Take user to high scores page
